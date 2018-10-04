fe89cfe7c7962221e8d0a00cd310542dec27d721
Vincent <vjdestefano500@gmail.com>
2018-10-04 14:52:10 -0400

    this feels good to script again

--- main.js
+++ main.js
@@ -2,30 +2,42 @@
 
 $(document).ready(function() {
     
-   let url = "https://via.placeholder.com/250x250"
+   let url = "https://via.placeholder.com/250x250";
+
+   let nameSpaces = [
+       "Test", "Bows", "Arrows",
+       "Archery Goods", "this Might work", "boats",
+       "Deer Mantales", "Guns", "Deer Gods",
+       "Something Something Guns","im running out of ideas","LUL",
+       "item - 25", "should this be here?", "item - 24", "this is item - 1", "test123", "hmm maybe i should get some lunch", "item - ID",
+       "is this enough?", "this might be right","hahahah", "fishing with the boys", "would you like some lunch?","I've lost the will"
+    
+    
+    ]
   
-for (let i = 0; i < 25; i++) {
+    for (let i = 0; i < 25; i++) {
    
-    let divs = $("<div>");
-    divs.addClass("col-4");
-    divs.attr("style","padding-left:5%; padding-bottom:2%;");
+        let divs = $("<div>");
+        divs.addClass("col-4");
+        divs.attr("style","padding-left:5%; padding-bottom:2%;");
 
-    let img = $("<img>");
-    img.addClass("responsive");
-    img.attr("src",url);
+        let img = $("<img>");
+        img.addClass("responsive");
+        img.attr("src",url);
 
-    let p = $("<p>");
-    p.addClass("lead");
-    p.html("This is a test and can be modified");
+        let p = $("<p>");
+        p.addClass("lead");
+        p.html(nameSpaces[i])
+        // p.html("This is a test and can be modified");
 
 
-    divs.append(img);
+        divs.append(img);
 
-    divs.append(p);
+        divs.append(p);
 
-    $("#selling-items").append(divs);
+        $("#selling-items").append(divs);
 
-    }
+        }
 
  
 });
